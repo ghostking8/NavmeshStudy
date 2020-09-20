ed3b7383047e0bb511144a782cb09d1a900b4ac5
ghostking8 <249540690@qq.com>
2020-09-20 14:56:26 +0800

    NavMesh项目

---
+++ NavMeshStudy/Assets/CubeMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    private bool direction = true;
+    private float position_z_start;
+     public float position_z_max = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position_z_start = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == true)
+        {
+            transform.position += new Vector3(0, 0, speed);
+
+
+            if (transform.position.z >= (position_z_start + position_z_max))
+            {
+                direction = false;
+            }
+        }
+        else
+        {
+            transform.position -= new Vector3(0, 0, speed);
+
+            if (transform.position.z <= position_z_start)
+            {
+                direction = true;
+            }
+        }        
+    }
+}
---
+++ NavMeshStudy/Assets/move.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class move : MonoBehaviour
+{
+    //public GameObject Dest;
+
+    public Camera cam;
+    private NavMeshAgent agent;
+
+    private Animation anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //agent.SetDestination(Dest.transform.position);
+
+        anim = GetComponent<Animation>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hit;
+            if(Physics.Raycast(ray,out Hit))
+            {
+                agent.SetDestination(Hit.point);
+            }
+                
+        }
+
+        if(agent.isOnOffMeshLink)
+        {
+            agent.CompleteOffMeshLink();
+        }
+
+        //判断是否正在移动
+        if(agent.velocity.sqrMagnitude==0)
+        {
+            anim.CrossFade("stand");
+        }
+        else
+        {
+            anim.CrossFade("walk");
+        }
+
+    }
+
+
+
+}
